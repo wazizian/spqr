366cc24b09e59df4dbc022564e64716089775260
Waiss Azizian <waiss.azizian@ens.fr>
2021-06-21 18:35:59 +0200

    Update tests for wdro estimators

--- spqr/tests/test_wdr_estimators.py
+++ spqr/tests/test_wdr_estimators.py
@@ -6,16 +6,13 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 from ..wdr_estimators import WDRLogisticRegression
+from ..estimators     import  DRLogisticRegression
 
-N = 100
-D = 10
-DELTA = 20
+N = 1000
+D = 50
 
 @pytest.fixture
 def dataset():
-#     centers = np.stack([np.zeros(D), DELTA*np.ones(D)], axis=0)
-#     assert centers.shape == (2, D)
-
     x, y, centers = make_blobs(n_samples=N, n_features=D, centers=2, return_centers=True)
 
     assert x.shape == (N, D)
@@ -27,10 +24,10 @@ def dataset():
     return *splitted_dataset, centers
 
 @pytest.mark.parametrize("rho,kappa", [
+    (0.1, 1),
     (1/4,2),
     (1/2,2),
     (1,2), 
-    pytest.param(1.5,2, marks=pytest.mark.xfail()),
     ])
 @pytest.mark.parametrize("mu", [0.1, 1, 10, 100])
 @pytest.mark.parametrize("mu_norm", [0.1, 1, 10, 100])
@@ -39,13 +36,12 @@ def test_WDRLogisticRegression(dataset, rho, kappa, mu, mu_norm):
 
     estimator = WDRLogisticRegression(rho, kappa, mu, mu_norm)
     estimator.fit(x_train, y_train, verbose_mode=True)
+
+    assert estimator.algorithm.bfgs_result_object.success, \
+           estimator.algorithm.bfgs_result_object.message
     
     for x, y, label in [(x_train, y_train, 'train'), (x_test, y_test, 'test')]:
         y_predicted = estimator.predict(x)
         acc = accuracy_score(y, y_predicted, normalize=False)
         print(f"Accuracy {label} = {acc}/{len(x)}")
         assert acc == len(x)
-
-#TODO: test more, and what happens when rho -> 0
-
-
