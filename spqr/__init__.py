1c4a83ddcea64490ca86ec84c084b113fbb9c200
Waiss Azizian <waiss_azizian@apple.com>
2025-03-04 22:57:30 +0100

    Remove unused template files
      - Removed template-related files (_template.py, test_template.py, test_common.py)
      - Updated __init__.py to remove template imports
      - These templates were only example code and not part of the actual package functionality

--- spqr/__init__.py
+++ spqr/__init__.py
@@ -2,15 +2,11 @@
 .. moduleauthor:: Yassine LAGUEL
 """
 
-from ._template import TemplateEstimator
-from ._template import TemplateClassifier
-from ._template import TemplateTransformer
 from .oracle import OracleSubgradient, OracleSmoothGradient
 from .risk_optimization import RiskOptimizer
 from .estimators import DRLinearRegression, DRLogisticRegression
 
 from ._version import __version__
 
-__all__ = ['TemplateEstimator', 'TemplateClassifier', 'TemplateTransformer',
-           '__version__', 'OracleSubgradient', 'OracleSmoothGradient',  'RiskOptimizer',
+__all__ = ['__version__', 'OracleSubgradient', 'OracleSmoothGradient',  'RiskOptimizer',
            'DRLinearRegression', 'DRLogisticRegression']
